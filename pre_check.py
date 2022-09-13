4aa46c225f62d7a6f32647a63f0e770abc982d06
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-13 15:52:58 +0800

    Update pre_check.py

--- pre_check.py
+++ pre_check.py
@@ -3,17 +3,19 @@ import os
 import datetime
 from loguru import logger
 
-sub_path = '/sub/' #默认存放订阅源的文件夹
+sub_path = 'sub' #默认存放订阅源的文件夹名称
 today = datetime.datetime.today()
-path_year = sub_path+str(today.year)
-path_mon = sub_path+path_year+'/'+str(today.month)
-path_yaml = sub_path+path_mon+'/'+str(today.month)+'-'+str(today.day)+'.yaml'
+path_year = sub_path+'/'+str(today.year)
+path_mon  = sub_path+'/'+path_year+'/'+str(today.month)
+path_yaml = sub_path+'/'+path_mon+'/'+str(today.month)+'-'+str(today.day)+'.yaml'
 
 @logger.catch
 def pre_check():
+    folder = os.path.exists(sub_path)
+    if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
+        os.makedirs(sub_path) 
 
     folder = os.path.exists(path_year)
-
     if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
         os.makedirs(path_year) 
 
